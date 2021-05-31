f0176fd8068b7cda8cd7828df504d8c80f0722d5
AronKinne <aronkinne@gmail.com>
2021-05-31 13:16:49 +0200

    Add test project with first test

---
+++ WeInvest.Tests/Models/InvestorTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System.Windows.Media;
+using WeInvest.Models;
+
+namespace WeInvest.Tests.Models {
+    [TestFixture]
+    public class InvestorTests {
+
+        [Test]
+        public void Deposit_FirstTime() {
+            Investor investor = new Investor("Tester", Brushes.Black);
+            float amount = 10;
+
+            investor.Deposit(amount);
+
+            Assert.That(investor.ShareHistory.Count, Is.EqualTo(1));
+            Assert.That(investor.ShareHistory[0], Is.EqualTo(amount));
+        }
+
+    }
+}

e0a65cd6f27cce52e3c61cb7b4de8f3f9b9c96c1
Aron Kinne <26415955+AronKinne@users.noreply.github.com>
2021-07-09 23:23:53 +0200

    Add window drag functionality

--- WeInvest.WPF/ViewModels/ViewModelBase.cs
+++ WeInvest.WPF/ViewModels/ViewModelBase.cs
@@ -1,12 +1,25 @@
 ﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using WeInvest.WPF.Commands;
 
 namespace WeInvest.WPF.ViewModels {
     public class ViewModelBase : INotifyPropertyChanged {
+
+        #region INotifyPropertyChanged members
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string name = null) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+
+        public ICommand DragCommand { get; }
+
+        public ViewModelBase() {
+            DragCommand = new RelayCommand(p => ((Window)p).DragMove());
         }
 
     }
